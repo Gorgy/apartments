12ce5bf556a10f7c901fe9f31fd2bf2fa4e70f01
Alexey Belov <belovaa@lad24.ru>
2021-11-09 19:52:29 +0300

    feat: add types and comments

--- src/api/graphql/users/typeDefs.ts
+++ src/api/graphql/users/typeDefs.ts
@@ -2,35 +2,67 @@ import { DocumentNode } from 'graphql';
 import { gql } from 'apollo-server-hapi';
 
 const typeDefs: DocumentNode = gql`
+  """
+  Тип описывающи поля пользователя
+  """
   type Users {
+    # Уникальный идентификатор пользователя
     _id: ID!
+    # Номер квартиры пользователя
     apartment: Int!
+    # Имя пользователя
     firstName: String!
+    # Фамилия пользователя
     lastName: String!
+    # Лицевой счет пользователя
     account: String!
-    isAdmin: Boolean
+    # Флаг супер-пользователя
+    isAdmin: Boolean!
   }
 
+  """
+  Ответ от сервера на добавление нового пользователя
+  """
   type AddUserResponse {
+    # Код ответа (404 / 500 / 200)
     code: Int!
+    # Флаг удачности запроса
     success: Boolean!
+    # Сообщение об ошибке/успешности запроса
     message: String!
+    # Данные нового пользователя
     user: Users
   }
 
+  """
+  Параметры для создания нового пользователя
+  """
   input AddUserArguments {
+    # Номер квартиры пользователя
     apartment: Int!
+    # Имя пользователя
     firstName: String!
+    # Фамилия пользователя
     lastName: String!
+    # Лицевой счет пользователя
     account: String!
+    # Флаг супер пользователя
     isAdmin: Boolean
   }
 
+  """
+  Типы описывающие запросы на получени данных
+  """
   type Query {
+    # Получить всех пользователей
     getUsers: [Users!]!
   }
 
+  """
+  Типы описывающие запросы на изменение данных
+  """
   type Mutation {
+    # Добавить нового пользователя
     addUser(data: AddUserArguments!): AddUserResponse!
   }
 `;
